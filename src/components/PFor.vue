18e03175ed41666f52d1a6ed50755d1e0e54a54e
hydrogen1121 <hydrogen1121@gmail.com>
2021-08-29 08:03:46 +0900

    add: PFor.vue

---
+++ src/components/PFor.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- <ul>
+    <li v-for="(step, i) in steps" :class="{is-active: i < activeIndex}">
+      {{ step.text }}
+    </li>
+  </ul> -->
+  <div v-for="(step, index) in steps" :key="(step.message, index)">
+    {{ step.message }}
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    activeIndex: Number,
+  },
+  data() {
+    return {
+      steps: [
+        { message: "アカウント入力" },
+        { message: "プラン選択" },
+        { message: "支払い設定方法" },
+        { message: "登録完了" },
+      ],
+    };
+  },
+});
+</script>
+
+<style></style>
